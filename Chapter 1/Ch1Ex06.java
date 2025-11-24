c04c184802e7e64e1d27d1324932ddfaf13c35f8
Shahariar6172 <shahariaral36@gmail.com>
2025-11-24 14:04:13 +0600

    ex 7 complete

---
+++ Chapter 1/Ch1Ex06.java
@@ -0,0 +1,9 @@
+public class Ch1Ex06 {
+       public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i <10; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
